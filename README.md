a06be7af1e97d6b16c1b1200eeddc813ed5c89c2
BayanBanat <bayankbanat99@gmail.com>
2023-05-08 19:46:48 +0000

    madlib

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
+# LAB - Class 03
 # madlib-cli
+
+
+## Bayan Banat
+
+
+**How to initialize/run application**
+
+    python madlib_cli/madlib.py
+
+
+**Tests**
+using pytest to write and run tests for code
+using **TDD** process:
+
+- **import pytest**
+- create a file  "__init__.py"
+- creating test_prompt function for the prompt function using unit test function with patch fixture :
+    
+    ```
+   def test_prompt():
+    with patch('builtins.input', side_effect=["bbbb", "aaaa", "yyyy", "aaaa", "nnnnn"]):
+        actual = prompt(5)
+    expected = ("bbbb", "aaaa", "yyyy", "aaaa", "nnnnn")
+    assert actual == expected
+
+    ```
+testing by run **pytest**
+
+
+ 
+
--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,37 +1,91 @@
 import re
 
 def welcomeMessage():
-    print("welcom to us \n playing with me and inter an objective \n")
+    '''
+    This function to show the welcome message.
+    '''
+    return "welcom to us !!\nThe program prompts you to input words, and then generates a unique story by inserting these words into a pre-written tale.\nsimply!! run the program from the command line and follow the prompts. Have fun!"
 
-def read_tamplate(path):
+
+
+def read_template(path):
+    '''
+    This function to open a file and read it then return the text inside it.
+    '''
     try:
       with open(path , 'r') as f:
-        global file 
         file= f.read()
+      return file
     except FileNotFoundError:
-       print("missing.txt")
-       
+       raise FileNotFoundError("File not found: " + path)
+   
 
 def parse_template(text):
-    global new_text,tupleWord
+    '''
+    Takes a Madlib template as input and print a tuple of empty strings and a modified string with empty curly brace placeholders.
+
+    Arguments:
+        text (str): The Madlib template string to be parsed.
+
+    return:
+        tuple: A tuple containing empty strings and a modified string with empty curly brace placeholders.
+
+    Example:
+        parse_template("It was a {adjective} and {adjective} {noun}.")
+        Output: 
+          "It was a {} and {} {}."  
+          ("adjective", "adjective", "noun")
+            
+    '''
     new_text = re.sub(r'{.*?}', '{}', text)
     matchesWord = re.findall(r'{(.*?)}', text)
     tupleWord = tuple(matchesWord)
-    return (new_text,tupleWord)
-
-def prompt():
-    userInput=input("enter the word needs !!")
-    global tupleUser
-    tupleUser=tuple(userInput)
-
-def merge():
-    for word in new_text:
-       madlibText= word.insert(tupleUser)
-    print(madlibText) 
+    return  new_text,tupleWord
 
 
 
+def prompt(num_words):
+    '''
+    This function to prompt user to enter specific types of words then print it as a tuple. 
+    '''
+    tupleUser=()
+    for i in range(num_words) :
+      userInput=input("enter word {}!! ".format(i + 1))
+      tupleUser+=(userInput,)
+    return tupleUser
 
+
+
+def merge(text,wordTuple):
+    '''
+    Merge the given word tuple into the text to create a Madlib story.
+
+    Arguments:
+        text (str): The Madlib template string, with placeholders for the words.
+        wordTuple (tuple): The words to be inserted into the placeholders in the text.
+
+    return:
+        merge the The Madlib template string with The words in wordTuple.
+
+    Example:
+        merge("It was a {} and {} {}.", ("dark", "stormy", "night")
+        # Output: "It was a dark and stormy night."
+    '''
+    madlibText = text.format(*wordTuple)
+    return madlibText
+
+
+
+
+
+
+if __name__ ==" __main__":
+  welcomeMessage()
+  read_template("assets/original_text.txt") 
+  parse_template("It was a {Adjective} and {Adjective} {Noun}." )
+  prompt()
+  merge("It was a {Adjective} and {Adjective} {Noun}.",("dark", "stormy", "night"))
+  merge("It was a {} and {} {}.", ("dark", "stormy", "night"))
 
 
 
